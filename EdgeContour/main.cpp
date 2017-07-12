8b8910e6f75ff873160d56fa939659f7fc1e78cc
Lichun Zhang <lczhang2011@gmail.com>
2017-07-12 16:24:10 +0800

    Add the TrackEdge method in EdgeContour module

--- EdgeContour/main.cpp
+++ EdgeContour/main.cpp
@@ -48,10 +48,14 @@ int TestEdgeDetection(const char *inname, const char *outname, size_t index) {
                            reader->GetImHeight(), reader->GetImSlice());
             break;
         case 7:
+            flag = ::EdgeTrack(reader->GetImData(), reader->GetImWidth(),
+                               reader->GetImHeight(), reader->GetImSlice(), 0);
+            break;
+        case 8:
             flag = ::Fill(reader->GetImData(), reader->GetImWidth(), reader->GetImHeight(), reader->GetImSlice(),
                           reader->GetImWidth() / 2, reader->GetImHeight() / 2, 0);
             break;
-        case 8:
+        case 9:
             flag = ::Fill2(reader->GetImData(), reader->GetImWidth(), reader->GetImHeight(), reader->GetImSlice(),
                            reader->GetImWidth() / 2, reader->GetImHeight() / 2, 0);
             break;
@@ -84,8 +88,9 @@ int main(int argc, char *argv[]) {
               << "4: Gaussian Laplace Edge Detection\n"
               << "5: Contour\n"
               << "6: Trace Contour\n"
-              << "7: Seed fill\n"
-              << "8: Seed fill 2\n";
+              << "7: Trace Edge\n"
+              << "8: Seed fill\n"
+              << "9: Seed fill 2\n";
     size_t index = 0;
     std::cin >> index;
     return TestEdgeDetection(argv[1], argv[2], index);
